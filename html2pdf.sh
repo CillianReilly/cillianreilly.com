541265ef1d1ecdc448a1ecd8c28ad630c735b062
Cillian Reilly <cillianreilly@yahoo.com>
2022-08-09 03:49:02 +0100

    Comment

--- html2pdf.sh
+++ html2pdf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Use https://www.sodapdf.com/pdf-to-html in the meantime
 sed s:"img src=\"":"img src=\"/home/pi/git/creilly/cillianreilly.com/":g cv.html > temp.html
 
 wkhtmltopdf temp.html cv.pdf
